f08f29e9fa670a6daffb2cb4d072b8ab269e2fc1
Thore Tiemann <thore.tiemann@student.uni-luebeck.de>
2017-12-20 17:38:25 +0100

    Day-10.1 fertig

--- Day-10/KnotHash.java
+++ Day-10/KnotHash.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class KnotHash {
     
     public static int[] list = new int[256];
+    static int[] test = new int[] {0, 1, 2, 3, 4};
     
     public static void main(String[] args) {
 	int[] input = new int[]{227,169,3,166,246,201,0,47,1,255,2,254,96,3,97,144};
@@ -14,12 +15,16 @@ public class KnotHash {
 	for(int i = 0; i < input.length; ++i){
 	    reverse(pointer, input[i]);
 	    pointer += skip + input[i];
+	    skip++;
 	}
 	System.out.println(list[0] * list[1]);
     }
 
     public static void reverse(int pos, int len){
-	
-    }
-    
+	for(int i = 0; i < len / 2; ++i){
+	    int tmp = list[(pos + i) % list.length];
+	    list[(pos + i) % list.length] = list[(pos + len - i - 1) % list.length];
+	    list[(pos + len - i - 1) % list.length] = tmp;
+	}
+    }    
 }

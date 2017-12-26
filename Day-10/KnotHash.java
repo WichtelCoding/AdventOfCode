84ebff4b7d7f7d8e943559475c53d8f2ab700db5
Anika <a.oellerich@gmx.net>
2017-12-26 11:10:09 +0100

    adding day 10 and 11

---
+++ Day-10/KnotHash.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class KnotHash {
+
+    public static void main(String[] args) throws FileNotFoundException {
+	Scanner sc = new Scanner(new FileReader(".\\Day-9\\day9.in"));
+	
+	sc.close();
+    }
+
+}
